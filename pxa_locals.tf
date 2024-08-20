172d8ccb83c73f5f447aa91d59bac642e532e971
ajinkya1107 <165694988+ajinkya1107@users.noreply.github.com>
2024-08-21 01:25:57 +0530

    Update pxa_locals.tf

--- pxa_locals.tf
+++ pxa_locals.tf
@@ -7,6 +7,7 @@ locals {
       "bastion" : aws_security_group.sg_bastion.id
       "cicd" : aws_security_group.sg_cicd.id
       "mongo" : aws_security_group.sg_mongo.id
+      "ansible" : aws_security_group.ansible.id
     }
   }
 
