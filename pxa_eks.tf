10a08e204e3d1bf7f2d62923c5a7cf6722983c3f
ajinkya1107 <165694988+ajinkya1107@users.noreply.github.com>
2024-11-28 01:32:46 +0530

    updated

--- pxa_eks.tf
+++ pxa_eks.tf
@@ -160,6 +160,7 @@ data "aws_eks_cluster_auth" "eks" {
 }
 
 provider "kubernetes" {
+  count                  = var.eks.create ? 1 : 0
   host                   = aws_eks_cluster.eks[0].endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks[0].certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks[0].token
