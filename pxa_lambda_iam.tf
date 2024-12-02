548ef7b87e925c2d50d87c10780ae120cafbdf78
ajinkya1107 <165694988+ajinkya1107@users.noreply.github.com>
2024-12-02 18:01:26 +0530

    updated role

--- pxa_lambda_iam.tf
+++ pxa_lambda_iam.tf
@@ -1,39 +1,43 @@
 resource "aws_iam_role" "lambda_role" {
   name = "${local.pxa_prefix}-lambda-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
   tags = {
     Name        = "${local.pxa_prefix}-lambda-role"
-    Project     = "${local.pxa_project_name}"
+    Project     = local.pxa_project_name
     Customer    = var.PROJECT_CUSTOMER
     Environment = var.PROJECT_ENV
     Terraform   = true
   }
 }
 
+# Use data sources to confirm policy ARNs
+data "aws_iam_policy" "lambda_basic_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy" "lambda_vpc_access_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = data.aws_iam_policy.lambda_basic_execution.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_vpc_access_execution" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  policy_arn = data.aws_iam_policy.lambda_vpc_access_execution.arn
 }
 
 resource "aws_iam_policy" "lambda_secrets_manager_policy" {
@@ -41,15 +45,15 @@ resource "aws_iam_policy" "lambda_secrets_manager_policy" {
   description = "Policy to allow Lambda functions to access Secrets Manager secrets"
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Action = [
           "secretsmanager:GetSecretValue",
           "secretsmanager:DescribeSecret",
           "secretsmanager:ListSecrets"
-        ],
+        ]
         Resource = "arn:aws:secretsmanager:${var.AWS_REGION}:${data.aws_caller_identity.current.account_id}:secret:${local.pxa_prefix}-*"
       }
     ]
