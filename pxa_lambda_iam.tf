97a5e48005eaa7bf0a7bb929fbf7724add9d7809
ajinkya1107 <165694988+ajinkya1107@users.noreply.github.com>
2024-12-02 14:34:48 +0530

    updated role

--- pxa_lambda_iam.tf
+++ pxa_lambda_iam.tf
@@ -27,7 +27,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role      = [aws_iam_role.lambda_role.name]
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
