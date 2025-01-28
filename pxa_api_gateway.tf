107abe461792f4036d437f0c909815b96f7e5379
ajinkya1107 <165694988+ajinkya1107@users.noreply.github.com>
2025-01-28 18:14:20 +0530

    DO-778: CORS for API Gateway

--- pxa_api_gateway.tf
+++ pxa_api_gateway.tf
@@ -200,7 +200,6 @@ EOF
   }
 }
 
-
 # /api/authenticate
 resource "aws_api_gateway_resource" "authenticate_resource" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
@@ -247,6 +246,112 @@ resource "aws_api_gateway_integration" "geolocation_integration" {
   uri                     = var.api_gateway.geolocation_uri
 }
 
+# CORS for /event
+resource "aws_api_gateway_method" "event_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.event.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "event_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.event.id
+  http_method = aws_api_gateway_method.event_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "event_options_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.event.id
+  http_method             = aws_api_gateway_method.event_options.http_method
+  type                    = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "event_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.event.id
+  http_method = aws_api_gateway_method.event_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+# CORS for /authenticate
+resource "aws_api_gateway_method" "authenticate_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.authenticate_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "authenticate_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.authenticate_resource.id
+  http_method = aws_api_gateway_method.authenticate_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "authenticate_options_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.authenticate_resource.id
+  http_method             = aws_api_gateway_method.authenticate_options.http_method
+  type                    = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "authenticate_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.authenticate_resource.id
+  http_method = aws_api_gateway_method.authenticate_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.event_post,
@@ -256,7 +361,9 @@ resource "aws_api_gateway_deployment" "deployment" {
     aws_api_gateway_integration_response.event_post_400,
     aws_api_gateway_integration_response.event_post_401,
     aws_api_gateway_integration_response.event_post_403,
-    aws_api_gateway_integration_response.event_post_500
+    aws_api_gateway_integration_response.event_post_500,
+    aws_api_gateway_integration.event_options_integration,
+    aws_api_gateway_integration.authenticate_options_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 }
