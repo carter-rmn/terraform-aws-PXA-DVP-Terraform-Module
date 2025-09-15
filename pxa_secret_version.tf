0f7352caeb01b9b80f102eb084b8c17b6dd27b3c
prakhyath-shyftlabs <129062908+prakhyath-shyftlabs@users.noreply.github.com>
2025-09-15 20:00:31 +0530

    Update pxa_secret_version.tf

--- pxa_secret_version.tf
+++ pxa_secret_version.tf
@@ -13,7 +13,7 @@ resource "aws_secretsmanager_secret_version" "pxa_secret_terraform" {
       }
     }
     keyspace = {
-      name = local.keyspace_name
+      name = try(aws_keyspaces_keyspace.carter_analytics[0].name, var.keyspace.existing.name)
     }
     app_user = {
       access_key = aws_iam_access_key.user_keys["app"].id
