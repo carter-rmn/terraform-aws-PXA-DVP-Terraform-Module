4d03bc94e613e88881f268209eb1f8bc54f7e014
prakhyath-shyftlabs <129062908+prakhyath-shyftlabs@users.noreply.github.com>
2026-04-10 15:28:42 +0530

    removed Iam users

--- pxa_user.tf
+++ pxa_user.tf
@@ -1,21 +1,21 @@
-resource "aws_iam_user" "users" {
-  for_each = local.users
-  name     = "${local.pxa_prefix}-user-${each.key}"
+# resource "aws_iam_user" "users" {
+#   for_each = local.users
+#   name     = "${local.pxa_prefix}-user-${each.key}"
 
-  tags = {
-    Name        = "${local.pxa_prefix}-user-${each.key}"
-    Project     = local.pxa_project_name
-    Customer    = var.PROJECT_CUSTOMER
-    Environment = var.PROJECT_ENV
-    Terraform   = true
-  }
-}
+#   tags = {
+#     Name        = "${local.pxa_prefix}-user-${each.key}"
+#     Project     = local.pxa_project_name
+#     Customer    = var.PROJECT_CUSTOMER
+#     Environment = var.PROJECT_ENV
+#     Terraform   = true
+#   }
+# }
 
-resource "aws_iam_access_key" "user_keys" {
-  for_each   = aws_iam_user.users
-  user       = "${local.pxa_prefix}-user-${each.key}"
-  depends_on = [aws_iam_user.users]
-}
+# resource "aws_iam_access_key" "user_keys" {
+#   for_each   = aws_iam_user.users
+#   user       = "${local.pxa_prefix}-user-${each.key}"
+#   depends_on = [aws_iam_user.users]
+# }
 
 resource "aws_iam_service_specific_credential" "keyspaces_app_user" {
   user_name    = aws_iam_user.users["app"].name
