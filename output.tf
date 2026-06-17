b518757aa4a4ee48a4ec1ec232a6507a9e6604b7
sarthak <sarthak.agarwal@shyftlabs.io>
2026-06-17 22:13:40 +0530

    removed outputs

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "msk_cluster_name" {
-  description = "Name of the MSK cluster (empty string when MSK is not created)"
-  value       = var.msk.create ? aws_msk_cluster.main[0].cluster_name : ""
-}
-
-output "msk_cluster_arn" {
-  description = "ARN of the MSK cluster (null when MSK is not created)"
-  value       = var.msk.create ? aws_msk_cluster.main[0].arn : null
-}
